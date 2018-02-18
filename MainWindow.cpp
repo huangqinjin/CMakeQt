a06305d2d4c540be9145ed9b4c28b00e47e6ebea
huangqinjin <huangqinjin@gmail.com>
2018-02-19 02:23:32 +0800

    no need QOpenGLContext in QOpenGLWindow

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -3,17 +3,15 @@
 
 MainWindow::MainWindow()
 {
-	setSurfaceType(OpenGLSurface);
-	create();
-	qDebug() << context.create();
-	context.makeCurrent(this);
-	initializeOpenGLFunctions();
+}
+
+void MainWindow::initializeGL()
+{
+    initializeOpenGLFunctions();
+    glClearColor(0, 0, 0, 0);
 }
 
 void MainWindow::paintGL()
 {
-	context.makeCurrent(this);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	context.swapBuffers(this);
-	context.doneCurrent();
 }
--- MainWindow.h
+++ MainWindow.h
@@ -1,13 +1,15 @@
 #include <QOpenGLWindow>
 #include <QOpenGLFunctions>
 
-class MainWindow : public QOpenGLWindow, protected QOpenGLFunctions
+class MainWindow : public QOpenGLWindow,
+				   protected QOpenGLFunctions
 {
 	Q_OBJECT
-protected:
-	QOpenGLContext context;
 
 public:
 	MainWindow();
-	void paintGL() override;
+	
+protected:
+    void initializeGL() override;
+    void paintGL() override;
 };

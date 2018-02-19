0ee9da078d4a54c7f802d29a29a8501255a3d944
huangqinjin <huangqinjin@gmail.com>
2018-02-19 22:22:06 +0800

    box and transform

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,33 +1,148 @@
 #include "MainWindow.h"
+#include <QtGui>
 #include <QDebug>
+
+using namespace Eigen;
 
 MainWindow::MainWindow()
 {
+    T.matrix().setZero();
 }
 
 void MainWindow::initializeGL()
 {
     initializeOpenGLFunctions();
     glClearColor(0, 0, 0, 0);
+    glShadeModel(GL_SMOOTH);
+    if (!T(3, 3)) glEnable(GL_DEPTH_TEST);
+
+    const bool orth = true;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if (orth) glOrtho(-1, 1, -1, 1, 1, 3 /* -1 */);
+    else glFrustum(-1.0, 1.0, -1.0, 1.0, 1, 100);
+    glMatrixMode(GL_MODELVIEW);
+
+    T = Translation3f(0, 0, -2.f);
 }
 
 void MainWindow::resizeGL(int w, int h)
 {
     (void)w;
     (void)h;
-    update();
 }
 
 void MainWindow::paintGL()
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(-1, -1, 0.0);
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(1, 0.0, 0.0);
+    // Clear the screen.
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex3f(0.0, 1, 0.0);
+    {
+        // Unlike GLUT, glViewport should be here
+        auto sz = size();
+        auto s = static_cast<GLsizei>(std::min(sz.width(), sz.height()));
+        glViewport(0, 0, s, s);
+    }
+
+    
+    glLoadMatrixf(T.data());
+    glTranslatef(-0.5f, -0.5f, -0.5f);
+
+    GLfloat data[8][3] = {
+        { 0, 0, 0 },
+        { 1, 0, 0 },
+        { 1, 1, 0 },
+        { 0, 1, 0 },
+        { 0, 1, 1 },
+        { 0, 0, 1 },
+        { 1, 0, 1 },
+        { 1, 1, 1 },
+    };
+
+    glBegin(GL_QUADS);
+    glColor3fv(data[0]); glVertex3fv(data[0]);
+    glColor3fv(data[1]); glVertex3fv(data[1]);
+    glColor3fv(data[2]); glVertex3fv(data[2]);
+    glColor3fv(data[3]); glVertex3fv(data[3]);
+
+    glColor3fv(data[2]); glVertex3fv(data[2]);
+    glColor3fv(data[3]); glVertex3fv(data[3]);
+    glColor3fv(data[4]); glVertex3fv(data[4]);
+    glColor3fv(data[7]); glVertex3fv(data[7]);
+
+    glColor3fv(data[4]); glVertex3fv(data[4]);
+    glColor3fv(data[7]); glVertex3fv(data[7]);
+    glColor3fv(data[6]); glVertex3fv(data[6]);
+    glColor3fv(data[5]); glVertex3fv(data[5]);
+
+    glColor3fv(data[6]); glVertex3fv(data[6]);
+    glColor3fv(data[5]); glVertex3fv(data[5]);
+    glColor3fv(data[0]); glVertex3fv(data[0]);
+    glColor3fv(data[1]); glVertex3fv(data[1]);
+
+    glColor3fv(data[0]); glVertex3fv(data[0]);
+    glColor3fv(data[3]); glVertex3fv(data[3]);
+    glColor3fv(data[4]); glVertex3fv(data[4]);
+    glColor3fv(data[5]); glVertex3fv(data[5]);
+
+    glColor3fv(data[1]); glVertex3fv(data[1]);
+    glColor3fv(data[2]); glVertex3fv(data[2]);
+    glColor3fv(data[7]); glVertex3fv(data[7]);
+    glColor3fv(data[6]); glVertex3fv(data[6]);
     glEnd();
+
+    // Draw the scene:
+    //        glBegin(GL_TRIANGLES);
+    //        glColor3f(1.0f, 0.0f, 0.0f);
+    //        glVertex3f(-1, -1, 0.0);
+    //
+    //        glColor3f(0.0f, 1.0f, 0.0f);
+    //        glVertex3f(1, 0.0, 0.0);
+    //
+    //        glColor3f(0.0f, 0.0f, 1.0f);
+    //        glVertex3f(0.0, 1, 0.0);
+    //        glEnd();
+}
+
+void MainWindow::keyPressEvent(QKeyEvent* e)
+{
+}
+
+void MainWindow::keyReleaseEvent(QKeyEvent* e)
+{
+}
+
+void MainWindow::mousePressEvent(QMouseEvent* e)
+{
+    pos = e->localPos();
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent* e)
+{
+    if (e->button() == Qt::MiddleButton)
+    {
+        initializeGL();
+        update();
+    }
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent* e)
+{
+    QPointF dp = e->localPos() - pos;
+    if (e->buttons() == Qt::LeftButton)
+    {
+        Vector3f axis(dp.y(), dp.x(), 0);
+        T.linear() = AngleAxisf(axis.norm() * 0.01f, axis.normalized()) * T.linear();
+    }
+    else if(e->buttons() == Qt::RightButton)
+    {
+        Vector3f dir(dp.x(), -dp.y(), 0);
+        T.translation() += dir * 0.01f;
+    }
+    pos = e->localPos();
+    update();
+}
+
+void MainWindow::wheelEvent(QWheelEvent* e)
+{
 }
--- MainWindow.h
+++ MainWindow.h
@@ -1,7 +1,8 @@
-#include <QOpenGLWindow>
+#include <QOpenGLWidget>
 #include <QOpenGLFunctions_3_3_Compatibility>
+#include <Eigen/Geometry>
 
-class MainWindow : public QOpenGLWindow,
+class MainWindow : public QOpenGLWidget,
 				   protected QOpenGLFunctions_3_3_Compatibility
 {
 	Q_OBJECT
@@ -13,4 +14,15 @@ protected:
     void initializeGL() override;
     void resizeGL(int w, int h) override;
     void paintGL() override;
+
+    void keyPressEvent(QKeyEvent* e) override;
+    void keyReleaseEvent(QKeyEvent* e) override;
+    void mousePressEvent(QMouseEvent* e) override;
+    void mouseReleaseEvent(QMouseEvent* e) override;
+    void mouseMoveEvent(QMouseEvent* e) override;
+    void wheelEvent(QWheelEvent* e) override;
+
+private:
+    QPointF pos;
+    Eigen::Affine3f T;
 };
